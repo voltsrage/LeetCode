5bdd468bffaabba02dbeb432c3e8d9e9c2992761
voltsrage <electric5388@hotmail.com>
2025-03-17 00:09:41 +0800

    Add Valid Parentheses

--- LeetCode/obj/Debug/net6.0/LeetCode.AssemblyInfo.cs
+++ LeetCode/obj/Debug/net6.0/LeetCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed71b90df00af0a49071cff5a08b25c025253c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b9de17f5858475c7b393d60add150c3100ca8d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
