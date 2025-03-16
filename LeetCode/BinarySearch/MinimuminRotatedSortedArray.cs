3d30923dac3854801f73a76f446259126430f6a8
voltsrage <electric5388@hotmail.com>
2025-03-17 00:49:21 +0800

    Add Binary Search neetcode 75

---
+++ LeetCode/BinarySearch/MinimuminRotatedSortedArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BinarySearch
+{
+    public class MinimuminRotatedSortedArray
+    {
+        public static int Execute(int[] nums)
+        {
+            // If the array is null or empty, return -1
+            if (nums == null || nums.Length == 0)
+            {
+                return -1;
+            }
+
+            // if the last element is greater than the first element, the array is not rotated
+            if (nums[nums.Length - 1] > nums[0])
+            {
+                return nums[0];
+            }
+
+            // Set the left and right pointers
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                // Calculate the middle index
+                var mid = left + (right - left) / 2;
+
+                // If the middle element is greater than the right element, the minimum element is on the right side
+                if (nums[mid] > nums[right])
+                {
+                    // Move the left pointer to the right of the middle element
+                    left = mid + 1;
+                }
+                // If the middle element is less than or equal to the right element, the minimum element is on the left side
+                else
+                {
+                    // Move the right pointer to the middle element
+                    right = mid;
+                }
+            }
+
+            // Return the element at the left pointer because it will be the minimum element
+            return nums[left];
+        }
+    }
+}
---
+++ LeetCode/BinarySearch/SearchinRotatedSortedArray.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.BinarySearch
+{
+    public class SearchinRotatedSortedArray
+    {
+        public static int Execute(int[] array, int target)
+        { 
+            if(array == null || array.Length == 0)
+            {
+                return -1;
+            }
+
+            // Set the left and right pointers
+            var left = 0;
+            var right = array.Length - 1;
+
+            while (left <= right)
+            {
+                // Calculate the middle index
+                var mid = left + (right - left) / 2;
+
+                // If the middle element is the target, return the index
+                if (array[mid] == target)
+                {
+                    return mid;
+                }
+
+                // If the left element is less than or equal to the middle element, the left side is sorted
+                if (array[left] <= array[mid])
+                {
+
+                    // If the target is between the left and middle elements, search the left side
+                    if (array[left] <= target && target < array[mid])
+                    {
+                        right = mid - 1;
+                    }
+
+                    // Otherwise, search the right side
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+
+                // If the left side is not sorted, the right side must be sorted
+                else
+                {
+                    // If the target is between the middle and right elements, search the right side
+                    if (array[mid] < target && target <= array[right])
+                    {
+                        left = mid + 1;
+                    }
+                    // Otherwise, search the left side
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
